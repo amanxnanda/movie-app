d3608397a63aee753b52bc0c705381818880c620
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-05-09 12:09:42 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/amannanda/movieapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amannanda.movieapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
